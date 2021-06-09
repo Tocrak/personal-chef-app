16e4b44fc56468dfa0bf00ca5a67bbb47e847eae
Tocrak <virgiliustsirdea@yandex.ru>
2021-06-09 17:49:25 +0200

    setup the express server

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express'),
+      mongoose = require('mongoose'),
+      session = require('express-session'),
+
+      account = require('./routes/account'),
+      chef = require('./routes/chef'),
+    
+      app = express(),
+      port = process.env.PORT || 8090;
+
+// min atributes needed to connect to mongo atlas
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/personal-chef-app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  dbName: 'personal-chef-app'
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to mongo'));
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: 'wadica',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {}
+}));
+
+app.use('/api', account)
+app.use('/api', chef)
+
+app.listen(port, () => {
+    console.log('Server Started');
+})
