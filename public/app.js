64405e794c2a176b6a9bcb8cca9c0e2a33a0fd49
Tocrak <virgiliustsirdea@yandex.ru>
2021-06-10 19:57:19 +0200

    create generic sendRequest method, remove old request methods

--- public/app.js
+++ public/app.js
@@ -1,10 +1,46 @@
-const getCookieValue = (name) => (
-    document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
-    )
+const getCookieValue = (name) => (document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || '')
 
 var vue = new Vue({
 	el: '#app',
 	data: {
+        api: {
+            login: {
+                url: `${window.location.origin}/api/login`,
+                method: "POST",
+                require_auth: false,
+            },
+            register: {
+                url: `${window.location.origin}/api/register`,
+                method: "POST",
+                require_auth: false,
+            },
+            logout: {
+                url: `${window.location.origin}/api/logout`,
+                method: "POST",
+                require_auth: true,
+            },
+            update_password: {
+                url: `${window.location.origin}/api/updatePassword`,
+                method: "PATCH",
+                require_auth: true,
+            },
+            submit_info: {
+                url: `${window.location.origin}/api/createMenu`,
+                method: "POST",
+                require_auth: true,
+                no_body_encoding: true,
+            },
+            delete_account: {
+                url: `${window.location.origin}/api/deleteAccount`,
+                method: "DELETE",
+                require_auth: true,
+            },
+            load_menu: {
+                url: `${window.location.origin}/api/loadMenu`,
+                method: "GET",
+                require_auth: true,
+            },
+        },
 		modal_active: false,
         modal_type: "",
         week_menu: {}
@@ -15,7 +51,7 @@ var vue = new Vue({
         if (getCookieValue('user') === "") {
             this.enableModal('login');
         } else {
-            await this.load();
+            await this.sendRequest('load_menu');
         }
 	},
 
@@ -30,207 +66,59 @@ var vue = new Vue({
             }
             return data;
         },
-        processModalAction: function(type, data) {
+        processSuccessfulRequest: async function(type, response) {
             switch (type) {
                 case "login":
-                    this.login(data);
-                break;
                 case "register":
-                    this.register(data);
-                break;
-                case "reset_password":
-                    
-                break;
-                case "update_password":
-                    this.updatePassword(data);
-                break;
-                case "submit_info":
-                    this.createMenu(data);
+                    await this.sendRequest('load_menu');
                 break;
                 case "delete_account":
-                    this.deleteAccount(data);
+                case "update_password":
+                    window.location.reload()
+                break;
+                case "load_menu":
+                case "submit_info":
+                    await response.json().then(data => {
+                        this.week_menu = data;
+                    })
                 break;
             }
-            console.log('processing ', data)
-        },
-        logout: async function() {
-            const url = `${window.location.origin}/api/logout`;
-
-			try {
-				let response = await fetch(url, {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json',
-						'Access-Control-Allow-Origin': '*',
-                        'Auth-Token': getCookieValue("token")
-					}
-				});
-
-				if (response.status == 200) {
-					window.location.reload()
-				} else {
-					alert('Response error: ' + response.status);
-				}
-			} catch (e) {
-				alert(e);
-			}
-        },
-        load: async function() {
-
-			const url = `${window.location.origin}/api/load`;
             
-			try {
+        },
+        sendRequest: async function(type, data) {
+            const url = this.api[type].url;
+
+            const headers = {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            }
+
+            if (!this.api[type].no_body_encoding) {
+                data = this.encodeRequestBody(data)
+            }
+
+            if (this.api[type].require_auth) {
+                headers['Auth-Token'] = getCookieValue("token");
+            }
+
+            try {
 				let response = await fetch(url, {
-					method: 'GET',
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json',
-						'Access-Control-Allow-Origin': '*',
-                        'Auth-Token': getCookieValue("token")
-					}
-				});
-
-				if (response.status == 200) {
-					await response.json().then(data => {
-						this.week_menu = data;
-					})
-				} else {
-					alert('Response error: ' + response.status);
-				}
-			
-			} catch (e) {
-				alert(e);
-			}
-		},
-        login: async function(credentials) {
-
-			const url = `${window.location.origin}/api/login`;
-
-			try {
-				let response = await fetch(url, {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json',
-						'Access-Control-Allow-Origin': '*'
-					},
-                    body: JSON.stringify(this.encodeRequestBody(credentials))
-				});
-
-				if (response.status == 200) {
-					this.modal_active = false;
-                    await this.load();
-				} else {
-					alert('Response error: ' + response.status);
-				}
-			} catch (e) {
-				alert(e);
-			}
-		},
-        register: async function(credentials) {
-
-			const url = `${window.location.origin}/api/register`;
-
-			try {
-				let response = await fetch(url, {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json',
-						'Access-Control-Allow-Origin': '*'
-					},
-                    body: JSON.stringify(this.encodeRequestBody(credentials))
-				});
+					method: this.api[type].method,
+					headers: headers,
+                    body: JSON.stringify(data)
+				})
 
 				if (response.status == 200) {
                     this.modal_active = false;
-                    await this.load();
+                    await this.processSuccessfulRequest(type, response);
 				} else {
 					alert('Response error: ' + response.status);
 				}
 			} catch (e) {
 				alert(e);
 			}
-		},
-        updatePassword: async function(credentials) {
-
-			const url = `${window.location.origin}/api/updatePassword`;
-
-			try {
-				let response = await fetch(url, {
-					method: 'PATCH',
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json',
-						'Access-Control-Allow-Origin': '*',
-                        'Auth-Token': getCookieValue("token")
-					},
-                    body: JSON.stringify(this.encodeRequestBody(credentials))
-				});
-
-				if (response.status == 200) {
-					this.modal_active = false;
-				} else {
-					alert('Response error: ' + response.status);
-				}
-			} catch (e) {
-				alert(e);
-			}
-		},
-        deleteAccount: async function(credentials) {
-
-			const url = `${window.location.origin}/api/deleteAccount`;
-
-			try {
-				let response = await fetch(url, {
-					method: 'DELETE',
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json',
-						'Access-Control-Allow-Origin': '*',
-                        'Auth-Token': getCookieValue("token")
-					},
-                    body: JSON.stringify(credentials)
-				});
-
-				if (response.status == 200) {
-					window.location.reload()
-				} else {
-					alert('Response error: ' + response.status);
-				}
-			} catch (e) {
-				alert(e);
-			}
-		},
-        createMenu: async function(data) {
-
-			const url = `${window.location.origin}/api/createMenu`;
-
-			try {
-				let response = await fetch(url, {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json',
-						'Access-Control-Allow-Origin': '*',
-                        'Auth-Token': getCookieValue("token")
-					},
-                    body: JSON.stringify(data)
-				});
-
-				if (response.status == 200) {
-					this.modal_active = false;
-                    await response.json().then(data => {
-						this.week_menu = data;
-					})
-				} else {
-					alert('Response error: ' + response.status);
-				}
-			} catch (e) {
-				alert(e);
-			}
-		},
+        },
 	}
 
 });
